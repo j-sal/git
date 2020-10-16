79ab270a6c445455e193400da2e6d37750d4b2c7
JoeyS <jgsal@yahoo.com>
2020-10-15 18:27:31 -0600

    psuh: display the top of origin/master
    Using pp_commit_easy to pretty-print the commit.
    
    Signed-off-by: JoeyS <jgsal@yahoo.com>

--- builtin/psuh.c
+++ builtin/psuh.c
@@ -1,35 +1,47 @@
 #include "builtin.h"
 #include "config.h"
 #include "wt-status.h"
+#include "commit.h"
+#include "pretty.h"
 
 int cmd_psuh(int argc, const char **argv, const char *prefix)
 {
 	struct wt_status status;
+	struct commit *c = NULL;
+	struct strbuf commitline = STRBUF_INIT;
 	const char *cfg_name;
 	int i;
 
 	printf(_("Another pony saying hello goes here!\n"));
 
-	printf(Q_("Your args (there is %d):\n",
+	printf(Q_("\nYour args (there is %d):\n",
 		  "Your args (there are %d):\n",
 		  argc),
 		argc);
 	for (i = 0; i < argc; i++)
 		printf("%d: %s\n", i, argv[i]);
 
-	printf(_("Your current working directory:\n<top-level>%s%s\n"),
+	printf(_("\nYour current working directory:\n<top-level>%s%s\n"),
 		prefix ? "/" : "", prefix ? prefix : "");
 
 	git_config(git_default_config, NULL);
 	if (git_config_get_string_tmp("user.name", &cfg_name) > 0)
 		printf(_("No name is found in config\n"));
 	else
-		printf(_("Your name: %s\n"), cfg_name);
+		printf(_("\nYour name: %s\n"), cfg_name);
 
 	wt_status_prepare(the_repository, &status);
 	git_config(git_default_config, &status);
 
-	printf(_("Your current branch: %s\n"), status.branch);
+	printf(_("\nYour current branch: %s\n"), status.branch);
+
+
+	c = lookup_commit_reference_by_name("origin/master");
+
+	if (c != NULL) {
+		pp_commit_easy(CMIT_FMT_ONELINE, c, &commitline);
+		printf(_("Current commit: %s\n\n"), commitline.buf);
+	}
 
 	return 0;
 }
